b77b941ed68f800c96d0f065ba276227bcc46756
Vu Le <leducvuvietnam@gmail.com>
2024-01-28 00:25:19 +0700

    added NotionPage render

--- app/components/NotionPage.tsx
+++ app/components/NotionPage.tsx
@@ -1,51 +1,132 @@
 "use client";
-import * as React from "react";
-import { NotionRenderer } from "react-notion-x";
-import "react-notion-x/src/styles.css";
-import { Code } from "react-notion-x/build/third-party/code";
-import { Collection } from "react-notion-x/build/third-party/collection";
-import { Equation } from "react-notion-x/build/third-party/equation";
-import { Modal } from "react-notion-x/build/third-party/modal";
-import { Pdf } from "react-notion-x/build/third-party/pdf";
 import dynamic from "next/dynamic";
+import Image from "next/image";
+import Link from "next/link";
+import { getPageTitle } from "notion-utils";
 
-export default function NotionPage({ recordMap }) {
-  const Code = dynamic(() =>
-    import("react-notion-x/build/third-party/code").then((m) => m.Code)
-  );
-  const Collection = dynamic(() =>
-    import("react-notion-x/build/third-party/collection").then(
-      (m) => m.Collection
-    )
-  );
-  const Equation = dynamic(() =>
-    import("react-notion-x/build/third-party/equation").then((m) => m.Equation)
-  );
-  const Pdf = dynamic(
-    () => import("react-notion-x/build/third-party/pdf").then((m) => m.Pdf),
-    {
-      ssr: false,
+import { Block, ExtendedRecordMap } from "notion-types";
+import { NotionRenderer } from "react-notion-x";
+
+import Head from "next/head";
+
+const Code = dynamic(() =>
+  import("react-notion-x/build/third-party/code").then((m) => m.Code)
+);
+const Collection = dynamic(() =>
+  import("react-notion-x/build/third-party/collection").then(
+    (m) => m.Collection
+  )
+);
+const Equation = dynamic(() =>
+  import("react-notion-x/build/third-party/equation").then((m) => m.Equation)
+);
+// const Pdf = dynamic(
+//   () => import("react-notion-x/build/third-party/pdf").then((m) => m.Pdf),
+//   {
+//     ssr: false,
+//   }
+// );
+const Modal = dynamic(
+  () => import("react-notion-x/build/third-party/modal").then((m) => m.Modal),
+  {
+    ssr: false,
+  }
+);
+
+export function mapImageUrl(url: string, block: Block): string | null {
+  if (!url) {
+    return null;
+  }
+
+  if (url.startsWith("data:")) {
+    return url;
+  }
+
+  // more recent versions of notion don't proxy unsplash images
+  if (url.startsWith("https://images.unsplash.com")) {
+    return url;
+  }
+
+  try {
+    const u = new URL(url);
+
+    if (
+      u.pathname.startsWith("/secure.notion-static.com") &&
+      u.hostname.endsWith(".amazonaws.com")
+    ) {
+      if (
+        u.searchParams.has("X-Amz-Credential") &&
+        u.searchParams.has("X-Amz-Signature") &&
+        u.searchParams.has("X-Amz-Algorithm")
+      ) {
+        // if the URL is already signed, then use it as-is
+        return url;
+      }
     }
-  );
-  const Modal = dynamic(
-    () => import("react-notion-x/build/third-party/modal").then((m) => m.Modal),
-    {
-      ssr: false,
-    }
-  );
-  return (
-    <NotionRenderer
-      recordMap={recordMap}
-      darkMode={false}
-      fullPage={true}
-      components={{
-        Code,
-        Collection,
-        Equation,
-        Modal,
-        Pdf,
-      }}
-    />
-  );
+  } catch {
+    // ignore invalid urls
+  }
+
+  if (url.startsWith("/images")) {
+    url = `https://www.notion.so${url}`;
+  }
+
+  url = `https://www.notion.so${
+    url.startsWith("/image") ? url : `/image/${encodeURIComponent(url)}`
+  }`;
+
+  const notionImageUrlV2 = new URL(url);
+  let table = block.parent_table === "space" ? "block" : block.parent_table;
+  if (table === "collection" || table === "team") {
+    table = "block";
+  }
+  notionImageUrlV2.searchParams.set("table", table);
+  notionImageUrlV2.searchParams.set("id", block.id);
+  notionImageUrlV2.searchParams.set("cache", "v2");
+
+  url = notionImageUrlV2.toString();
+
+  return url;
 }
-// https://github.com/NotionX/react-notion-x/issues/511
+
+export const NotionPage = ({
+  recordMap,
+  rootPageId,
+}: {
+  recordMap: ExtendedRecordMap;
+  rootPageId?: string;
+}) => {
+  if (!recordMap) {
+    return null;
+  }
+  const title = getPageTitle(recordMap);
+
+  console.log("title is");
+  console.log(title);
+
+  return (
+    <>
+      <Head>
+        <meta name={title} content={title} />
+        <title>{title}</title>
+      </Head>
+
+      <NotionRenderer
+        recordMap={recordMap}
+        fullPage={false}
+        darkMode={false}
+        rootPageId={rootPageId}
+        components={{
+          // NOTE (transitive-bullshit 3/12/2023): I'm disabling next/image for this repo for now because the amount of traffic started costing me hundreds of dollars a month in Vercel image optimization costs. I'll probably re-enable it in the future if I can find a better solution.
+          // nextImage: Image,
+          nextLink: Link,
+          Code,
+          Collection,
+          Equation,
+          // Pdf,
+          Modal,
+        }}
+      />
+    </>
+  );
+};
